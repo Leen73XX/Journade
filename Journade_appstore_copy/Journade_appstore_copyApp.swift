b1b0682cebe709179e41bd2ef939c8a72810fc9b
Leen73XX <leen.mej@gmail.com>
2024-11-07 23:07:07 +0300

    Initial Commit

---
+++ Journade_appstore_copy/Journade_appstore_copyApp.swift
@@ -0,0 +1,17 @@
+//
+//  Journade_appstore_copyApp.swift
+//  Journade_appstore_copy
+//
+//  Created by Leen Almejarri on 05/05/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct Journade_appstore_copyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
